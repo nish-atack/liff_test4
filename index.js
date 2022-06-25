f6234489092fe252763c0bc5cd1779f59c208f7b
nish-atack <106975240+nish-atack@users.noreply.github.com>
2022-06-25 13:22:06 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ $(function () {
     $('form').submit(function () {
         var reasons = $('textarea[name="reasons"]').val();
         var time1 = $('input[name="time1"]').val();
-        var num1 = $('input[name="num1"]').val();
-        var breed = $('input[name="breed"]').val();
+        var num1 = $('input[name="num2"]').val();
+        var breed = $('input[name="breed2"]').val();
         
         // var date = $('input[name="date"]').val();
         // var tool = $('input[name="tool"]').val();
